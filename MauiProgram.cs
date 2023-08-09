113098428e756144657a76609c929d2372012f27
K_Ketchup <121308517+K-Ketchup@users.noreply.github.com>
2023-08-09 07:47:10 -0700

    Maui Maps

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -13,7 +13,9 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			})
+            .UseMauiMaps();
+
 
 #if DEBUG
 		builder.Logging.AddDebug();
