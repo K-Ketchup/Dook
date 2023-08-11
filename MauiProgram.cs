a669be7df721e4ceaf42a15f4521a66d1a1a18dc
K_Ketchup <121308517+K-Ketchup@users.noreply.github.com>
2023-08-11 06:55:08 -0700

    Maps

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -9,12 +9,12 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
+            .UseMauiMaps()
+            .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			})
-            .UseMauiMaps();
+			});
 
 
 #if DEBUG
