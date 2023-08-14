0d43ed4823078917c6a4e83e189e6abd1787e7ea
K_Ketchup <121308517+K-Ketchup@users.noreply.github.com>
2023-08-14 01:57:51 -0700

    Maui Maps

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,13 +1,11 @@
 ﻿namespace Dook;
-using Map = Microsoft.Maui.Controls.Maps.Map;
 
 public partial class MainPage : ContentPage
 {
 
 	public MainPage()
 	{
-        Map map = new Map();
-        Content = map;
+        InitializeComponent();
     }
 }
 
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,28 +1,27 @@
 ﻿using Microsoft.Extensions.Logging;
-using Microsoft.Maui.Controls.Maps;
-using Microsoft.Maui.Controls;
 
-namespace Dook;
-
-public static class MauiProgram
+namespace Dook
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-            .UseMauiMaps()
-            .UseMauiApp<App>().UseMauiMaps()
-            .ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                })
+                .UseMauiMaps();
 
+        #if DEBUG
+            builder.Logging.AddDebug();
+        #endif
 
-#if DEBUG
-		builder.Logging.AddDebug();
-#endif
-
-		return builder.Build();
-	}
+            return builder.Build();
+        }
+    }
 }
+
