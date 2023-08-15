89047967230981e39d82e7f659813bce9ae9a091
K_Ketchup <121308517+K-Ketchup@users.noreply.github.com>
2023-08-15 05:42:19 -0700

    Finally working on map stuffs!!!

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,11 +1,35 @@
 ﻿namespace Dook;
+using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
+using System.Diagnostics;
+using Map = Microsoft.Maui.Controls.Maps.Map;
 
 public partial class MainPage : ContentPage
 {
 
 	public MainPage()
 	{
+        GetLocation();
         InitializeComponent();
     }
+
+    private async void GetLocation()
+    {
+        Location location = await Geolocation.GetLocationAsync(new GeolocationRequest
+        {
+            DesiredAccuracy = GeolocationAccuracy.High,
+            Timeout = TimeSpan.FromSeconds(30)
+        });
+        if(location != null)
+        {
+            MapSpan mapSpan = new MapSpan(location, 0.01, 0.01);
+            Map map = new Map(mapSpan);
+        }
+
+        Debug.Print("Bruh");
+    }
+
+
 }
+
 
