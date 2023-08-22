41f0fd86fc5661f78df39b5106d29c407d6ddba7
K_Ketchup <121308517+K-Ketchup@users.noreply.github.com>
2023-08-22 11:41:29 -0700

    Some more MVVM

---
+++ ViewModel/DetailViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Dook.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dook.Services;
+
+namespace Dook.ViewModel
+{
+    public partial class DetailViewModel : BaseViewModel
+    {
+        public ObservableCollection<Review> Reviews { get; } = new();
+        public Command GetReviewsCommand { get; }
+        ReviewService reviewService;
+
+        public DetailViewModel(ReviewService reviewService)
+        {
+            Title = "Review Finder";
+            this.reviewService = reviewService;
+            GetReviewsCommand = new Command(async () => await GetReviewsAsync());
+        }
+        async Task GetReviewsAsync()
+        {
+            if (IsBusy)
+                return;
+
+            try
+            {
+                IsBusy = true;
+
+                var reviews = await reviewService.GetReviews();
+
+                if (Reviews.Count != 0)
+                    Reviews.Clear();
+
+                foreach (var review in reviews)
+                    Reviews.Add(review);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Unable to get reviews: {ex.Message}");
+                await Application.Current.MainPage.DisplayAlert("Error!", ex.Message, "OK");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
