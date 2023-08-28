f78e3c8d2647b5277f74a161eb427e93200beb38
K_Ketchup <121308517+K-Ketchup@users.noreply.github.com>
2023-08-27 23:59:35 -0700

    Map Implementation

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace Dook.ViewModel
 {
-    internal class MainViewModel
+    public partial class MainViewModel : BaseViewModel
     {
+        public string currentLocation;
+        public Command GetCurrentLocationCommand { get; }
+        public MainViewModel()
+        {
+            Title = "Map Controller";
+            GetCurrentLocationCommand = new Command(async () => await GetCurrentLocationAsync());
+        }
+
+        async Task GetCurrentLocationAsync()
+        {
+            if (IsBusy)
+                return;
+
+            try
+            {
+                IsBusy = true;
+                Location location = await Geolocation.Default.GetLastKnownLocationAsync();
+                if (location != null)
+                    currentLocation = $"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}";
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Unable to get current location: {ex.Message}");
+                await Application.Current.MainPage.DisplayAlert("Error!", ex.Message, "OK");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
     }
 }
