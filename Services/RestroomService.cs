ac925c1b10a933ff66f135ac237d77b93d8748c7
K_Ketchup <121308517+K-Ketchup@users.noreply.github.com>
2023-10-22 16:17:13 -0700

    SQLite debugging still

--- Services/RestroomService.cs
+++ Services/RestroomService.cs
@@ -23,13 +23,11 @@ namespace Dook.Services
             db = new SQLiteAsyncConnection(databasePath);
 
             await db.CreateTableAsync<Restroom>();
-            var wa = "wa";
         }
 
         public static async Task AddPinAsync(string name, string address, string username, Location location)
         {
             await InitAsync();
-            var wa = "wa";
             var restroom = new Restroom
             {
                 Name = name,
