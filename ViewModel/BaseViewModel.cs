025fa8a56b6bd6a28f2d69e55f3a01b3c57d5fdc
K_Ketchup <121308517+K-Ketchup@users.noreply.github.com>
2023-10-31 23:29:41 -0700

    Almost fixed DataBinding

--- ViewModel/BaseViewModel.cs
+++ ViewModel/BaseViewModel.cs
@@ -8,6 +8,11 @@ namespace Dook.ViewModel
 {
     public partial class BaseViewModel : ObservableObject
     {
+        public BaseViewModel()
+        {
+
+        }
+
         [ObservableProperty]
         [NotifyPropertyChangedFor(nameof(IsNotBusy))]
         bool isBusy;
