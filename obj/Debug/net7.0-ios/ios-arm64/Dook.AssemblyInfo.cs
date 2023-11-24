f27d3590df90e4ab9929a50c33d413dbf2440efc
K_Ketchup <121308517+K-Ketchup@users.noreply.github.com>
2023-11-24 13:59:47 -0800

    Fixed bugs related to HTTP requests

--- obj/Debug/net7.0-ios/ios-arm64/Dook.AssemblyInfo.cs
+++ obj/Debug/net7.0-ios/ios-arm64/Dook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e6937fb9dfa32d6fd9651d1307865e709d457404")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
